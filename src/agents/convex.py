8f9c1b0d847e1238c0eaf402219bb521c44e2842
Simon Yang <yzh503@live.com>
2023-07-08 19:40:29 +1000

    Fixing issues

--- src/agents/convex.py
+++ src/agents/convex.py
@@ -31,22 +31,17 @@ class ConvexAgent(Base):
 
         if len(self.queue) > 0:  # check if there's any operation left in the queue
             action = self.queue.pop(0)  # perform the operation and remove it from the queue
-            return action
-
+            return action + 1
+        
         new_placement = self.maximize_nuclear_norm(vm_cpu, vm_memory, vm_placement.copy())
 
         # figure out VM migrations and add them into queue
         for i in range(len(vm_placement)):
             if vm_placement[i] > -1 and new_placement[i] > -1 and vm_placement[i] != new_placement[i]:
-                self.queue.append((i, -1))  # remove operation
-                self.queue.append((i, new_placement[i]))  # place operation
+                self.queue.append(new_placement.copy()) 
+                new_placement[i] = -1  # remove the VM from the new placement
 
-        if len(self.queue) > 0:
-            action = self.queue.pop(0)  # if we have added some operations into the queue, perform the first one
-        else:
-            action = new_placement + 1  # if no operations were added, just apply the new placement
-
-        return action
+        return new_placement + 1
 
     # V is the number of VMs
     # P is the number of PMs
@@ -55,7 +50,9 @@ class ConvexAgent(Base):
     # X_values is the VxP matrix of VMs' placement
     def maximize_nuclear_norm(self, A, B, vm_placement):
 
-        V = self.env.config.vms
+        if np.count_nonzero(vm_placement > -2) == 0:
+            return vm_placement
+        
         P = self.env.config.pms
 
         M = np.zeros(shape=(self.env.config.vms, self.env.config.pms))
@@ -63,62 +60,64 @@ class ConvexAgent(Base):
             if pm > -1:
                 M[i, pm] = 1 
         
-        cols_to_optimize = np.ones(P, dtype=bool)
-        rows_to_optimize = np.argwhere(vm_placement > -2).flatten().tolist()
+        unavailable_pms = np.zeros(P, dtype=bool)
+        rows_to_optimize = vm_placement > -2 # variable rows
         rows_optimized = []
-        while len(rows_to_optimize) > 0:
-            i = 0
-            while i < len(rows_to_optimize):
-                v = rows_to_optimize[i]
-                var_columns = M[v, cols_to_optimize]
-                X = cvx.Variable((1, var_columns.size))
-                X.value = var_columns.reshape(1, -1)
 
-                if v == 0:
-                    rows = [X] + [M[1:, cols_to_optimize]]
-                elif v == V - 1:
-                    rows = [M[:v, cols_to_optimize]] + [X]
-                else: 
-                    rows = [M[:v, cols_to_optimize]] + [X] + [M[v+1:, cols_to_optimize]]
+        i = 0
+        while np.count_nonzero(rows_to_optimize) > 0 and np.count_nonzero(unavailable_pms) < P:
+            rows_formatted = []
+            row_variable = []
+            for i, row in enumerate(M[vm_placement > -2]): 
+                if rows_to_optimize[i]:
+                    Z = cvx.Variable((1, P))
+                    Z.value = row.reshape(1, -1)
+                    rows_formatted.append(Z)
+                    row_variable.append(True)
+                else:
+                    rows_formatted.append(row)
+                    row_variable.append(False)
 
-                rows_formatted = []
-                for r in rows:
-                    if isinstance(r, cvx.Variable):
-                        rows_formatted.append(r)  # cvxpy Variables can be added as is
-                    else:
-                        rows_formatted.extend(r.tolist())  # Numpy arrays need to be turned into lists
+            X = cvx.bmat(rows_formatted)
+            ones = np.ones(P).reshape(1, -1)
+            
+            mask = np.zeros_like(X.value, dtype=bool)
+            mask[np.ix_(np.array(row_variable), unavailable_pms)] = 1
 
-                X = cvx.bmat(rows_formatted)
-                constraints = [0 <= X, X <= 1, X.T @ np.ones(V) == 1, A @ X <= 1, B @ X <= 1]
-                objective = cvx.Minimize(cvx.norm(X, 'nuc'))
+            constraints = [0 <= X, 
+                        X <= 1, 
+                        ones @ X.T == 1, 
+                        A[vm_placement > -2] @ X <= 1, 
+                        B[vm_placement > -2] @ X <= 1,
+                        cvx.multiply(X, mask) == 0]
+            
+            objective = cvx.Minimize(cvx.norm(X, 'nuc'))
 
-                prob = cvx.Problem(objective, constraints)
-                prob.solve(solver=cvx.CVXOPT)
+            prob = cvx.Problem(objective, constraints)
+            prob.solve(solver=cvx.CVXOPT)
+            assert prob.status == cvx.OPTIMAL, prob.status
 
-                # For each row of X (VM), change the highest value to 1 and other values to 0
-                N = np.array(X.value)
+            X_full = M[vm_placement > -2]
+            X_opt = np.array(X.value)
 
-                max_index = np.argmax(N[v])
-                N[v, :] = 0
-                N[v, max_index] = 1
+            # Algorithm 2: VM Deployement 
+            for v, row in enumerate(X_opt): 
+                if rows_to_optimize[v]:
+                    p = np.argmax(row).flatten()[0]
+                    X_full[v, :] = 0
+                    X_full[v, p] = 1
+                    overloaded = np.logical_or(A[vm_placement > -2] @ X_full > 1, B[vm_placement > -2] @ X_full > 1)
+                    if np.count_nonzero(overloaded) > 0: 
+                        unavailable_pms[p] = True
+                        X_opt = np.delete(X_opt, p, axis=1)
+                    else: 
+                        rows_optimized.append((v, X_full[v]))
+                        rows_to_optimize[v] = False
+                    X_opt[v, :] = 0
 
-                N_full = np.copy(M)  # Start from a copy of M
-                N_full[v, cols_to_optimize] = N[v, cols_to_optimize] 
+            M[vm_placement > -2] = X_full[:]
 
-                # Exclude PMs that are or will be overloaded
-                underloaded = np.logical_and(A @ N_full <= 1, B @ N_full <= 1)
-                if np.count_nonzero(underloaded) == len(underloaded):
-                    rows_optimized.append(N_full[v])
-                    del rows_to_optimize[i]
-                else: 
-                    i += 1
-
-                cols_to_optimize = np.logical_and(underloaded, cols_to_optimize)
-
-                # Update placement
-                M = np.copy(N_full)
-        
-        for row in rows_optimized: 
+        for v, row in rows_optimized: 
             pm = np.argwhere(row == 1).flatten() # pm is the index of available PMs
             if pm.size == 1:
                 vm_placement[v] = pm[0]
@@ -126,4 +125,5 @@ class ConvexAgent(Base):
                 pass
             else:
                 raise Exception("VM is assigned to multiple PMs: ", pm)
+            
         return vm_placement
