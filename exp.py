b8ddcd18b033272389d07d02a0ab901773d7a3e2
Simon Yang <yzh503@live.com>
2023-08-13 21:00:49 +1000

    Update cpu count

--- exp.py
+++ exp.py
@@ -1,6 +1,6 @@
-cores = 20              # Number of cores to run experiments
+cores = 24             # Number of cores to run experiments
 multiruns = 10          # Number of runs to take average in experiments
-eval_steps = 150000       # Number of steps in an evaluation episode in experiments
+eval_steps = 200000       # Number of steps in an evaluation episode in experiments
 service_length = 1000   # Number of steps in a VM lifetime in experiments
 load = 1.0                # default system load
 pms = 100                # Number of PMs in experiments
